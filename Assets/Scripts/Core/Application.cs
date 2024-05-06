970e791f11d1eae927a2836a6d3b865d7e8fcda6
DESKTOP-JE0RQHM\Nika <adress@gmail.com>
2024-05-06 11:30:35 +0300

    import spine 3.8, add player and environment frefab

--- Assets/Scripts/Core/Application.cs
+++ Assets/Scripts/Core/Application.cs
@@ -1,7 +1,29 @@
-﻿namespace Core
+﻿using UnityEngine;
+
+namespace Core
 {
-    public class Application
+    public class Application : MonoBehaviour
     {
-        
+        private static Application _instance;
+
+        public static Application Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+
+                _instance = FindObjectOfType<Application>();
+
+                return _instance;
+            }
+        }
+
+        public void Exit()
+        {
+            UnityEngine.Application.Quit();
+        }
     }
 }
--- Assets/Scripts/UI/GameplayDialogController.cs
+++ Assets/Scripts/UI/GameplayDialogController.cs
@@ -1,7 +1,27 @@
-﻿namespace UI
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Application = Core.Application;
+
+namespace UI
 {
-    public class GameplayDialogController
+    public class GameplayDialogController : MonoBehaviour
     {
-        
+        [SerializeField] private Button _exitButton;
+
+        [SerializeField] private TextMeshProUGUI _playerScore;
+
+        private void Awake()
+        {
+            _exitButton.onClick.AddListener(OnExitButtonClick);
+            
+        }
+
+        private void OnExitButtonClick()
+        {
+            Application.Instance.Exit();
+            Debug.Log("Exit");
+        }
     }
 }
