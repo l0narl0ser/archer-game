abd28fada419fda41310af0c01907db0193093d0
Fedorenko Veronika <ronyaufim@icloud.com>
2024-05-09 14:56:18 +0300

    fix

--- Assets/Scripts/Game/ArrowShoot.cs
+++ Assets/Scripts/Game/ArrowShoot.cs
@@ -6,25 +6,15 @@ namespace Game
     {
         [SerializeField]
         private Transform _shotPoint;
-       
+
         [SerializeField]
         private GameObject _arrow;
 
-        [SerializeField]
-        private float _force;
-        
-        private void Update()
-        {
-            if (Input.GetMouseButtonUp(0))
-            {
-                Shoot();
-            }
-        }
 
-        private void Shoot()
+        public void Shoot(Vector2 force)
         {
             GameObject arrowInstantiate = Instantiate(_arrow, _shotPoint.position, _shotPoint.rotation);
-            arrowInstantiate.GetComponent<Rigidbody2D>().velocity = _shotPoint.right * _force;
+            arrowInstantiate.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
         }
     }
 }
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera _worldCamera;
+
+        [SerializeField]
+        private ArrowShoot _arrowShoot;
+
+        [SerializeField]
+        private Transform _arrow;
+
+        [SerializeField]
+        private TrajectoryController _trajectory;
+
+        [SerializeField]
+        private float _pushForce = 4f;
+        
+        private Vector3 _endPoint;
+        private Vector2 _direction;
+        private Vector2 _force;
+        private float _distance;
+        private bool _isDragging = false;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                _isDragging = false;
+                OnDragEnd();
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                _isDragging = true;
+                OnDragStart();
+            }
+
+            if (_isDragging)
+            {
+                OnDrag();
+            }
+        }
+
+        private void OnDragStart()
+        {
+            _trajectory.ShowTrajectory();
+        }
+
+        private void OnDrag()
+        {
+            _endPoint = _worldCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 currentMousePosition = _worldCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 moveDirection = (currentMousePosition - _arrowShoot.transform.position).normalized;
+            _direction = moveDirection * -1;
+            _distance = Vector3.Distance(_endPoint, _arrowShoot.transform.position);
+
+            float leanAngleRadians = Vector2.SignedAngle(Vector2.right, moveDirection);
+            if (leanAngleRadians < 90 && leanAngleRadians > -90)
+            {
+                _direction *= -1;
+            }
+
+            _force = _direction * _distance * _pushForce;
+
+            _trajectory.UpdateDots(_arrow.position, _force);
+        }
+
+        private void OnDragEnd()
+        {
+            _arrowShoot.Shoot(_force);
+            _trajectory.HideTrajectory();
+        }
+    }
+}
